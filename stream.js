687b087dec637a7e528f0e79db4247b60278745e
Adriaan Pelzer <adriaan.pelzer@telegraph.co.uk>
2016-02-01 12:52:19 +0000

    Make stream return full list

--- stream.js
+++ stream.js
@@ -7,11 +7,21 @@ module.exports = R.curry ( function ( s3Parms, listObjectParms ) {
     var S = new A.S3 ( s3Parms );
 
     var objectStream = function ( listObjectParms ) {
-        return W ( S, 'listObjects' )( listObjectParms )
-            .flatMap ( function ( output ) {
-                return H ( output.Contents )
-                    .concat ( output.IsTruncated ? objectStream ( R.merge ( listObjectParms, R.compose ( R.createMapEntry ( 'Marker' ), R.prop ( 'Key' ), R.last, R.prop ( 'Contents' ) )( output ) ) ) : H ( [] ) );
-            } );
+        return H ( function ( push, next ) {
+            W ( S, 'listObjects' )( listObjectParms )
+                .errors ( R.unary ( push ) )
+                .each ( function ( output ) {
+                    for ( var i = 0; i < output.Contents.length; i++ ) {
+                        push ( null, output.Contents[i] );
+                    }
+
+                    if ( output.IsTruncated ) {
+                        return next ( objectStream ( R.merge ( listObjectParms, R.compose ( R.createMapEntry ( 'Marker' ), R.prop ( 'Key' ), R.last, R.prop ( 'Contents' ) )( output ) ) ) );
+                    }
+
+                    return push ( null, H.nil );
+                } );
+        } );
     };
 
     return objectStream ( listObjectParms );
